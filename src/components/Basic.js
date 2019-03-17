9864c1d1fb93e8fac4d2ed18302449711b8ade62
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-17 17:27:11 +0500

    Props with an Image Child Component

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Banana from './Banana';
 
 class Basic extends Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <Text>M A Jinnah University</Text>
+                <Banana imageURL="https://www.jinnah.edu/wp-content/uploads/2016/02/maj-log.png" />
             </View>
         );
     }
