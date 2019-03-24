c9bc5bcc830649d7df71b64d4858e2b5ffe50db3
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-24 16:51:31 +0500

    Adjusting imports in Basic.js

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 // import Banana from './Banana';
 import Header from './Header';
-import ProfilePic from './ProfilePic';
 import Content from './Content';
 import Footer from './Footer';
 import ProfilePic from './ProfilePic';
@@ -12,7 +11,7 @@ class Basic extends Component {
         return (
             <ScrollView>
                 <Header headerText="My Personal Information" />
-                {/* <ProfilePic imageURL="https://timesofindia.indiatimes.com/thumb/msid-65021879,imgsize-16139,width-400,resizemode-4/65021879.jpg" /> */}
+                <ProfilePic imageURL="https://timesofindia.indiatimes.com/thumb/msid-65021879,imgsize-16139,width-400,resizemode-4/65021879.jpg" />
                 <Content contentHeading="Professional Exp" text="2018 -- 2019: Worked with Some Company as a React Native Dev" />
                 <Content contentHeading="Education" text="2017 -- 2018: Diploma in React Native Dev" />
                 <Content contentHeading="Education" text="2017 -- 2018: Diploma in React Native Dev" />
