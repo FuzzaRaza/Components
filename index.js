3e0d6894c55a77ad6b8fbb2626bd52a671ae1a24
MAJU STUDENT <majustudent@jinnah.edu>
2019-03-31 18:53:05 +0500

    Trying updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import Props from './src/components/Props';
 import State from './src/components/State';
 import Fetching from './src/components/Fetching';
 import SectionWiseView from './src/components/SectionWiseView';
-import { name as appName } from './app.json';
+
 import ScrollView from './src/components/ScrollView';
 import HandlingTouch from './src/components/HandlingTouch';
 import TextHandling from './src/components/TextHandling';
@@ -16,5 +16,6 @@ import FlexboxLayout from './src/components/FLexboxLayout';
 import HeightWidth from './src/components/HeightWidth';
 import Styling from './src/components/Styling';
 import Reactnav from './src/components/Reactnav';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => ScrollView);
+AppRegistry.registerComponent(appName, () => Reactnav);
--- src/components/Reactnav.js
+++ src/components/Reactnav.js
@@ -1,22 +1,21 @@
-import com.facebook.react.ReactActivity;
-+ import com.facebook.react.ReactActivityDelegate;
-+ import com.facebook.react.ReactRootView;
-+ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-public class MainActivity extends ReactActivity {
-
-  @Override
-  protected String getMainComponentName() {
-    return "Example";
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
   }
-
-+  @Override
-+  protected ReactActivityDelegate createReactActivityDelegate() {
-+    return new ReactActivityDelegate(this, getMainComponentName()) {
-+      @Override
-+      protected ReactRootView createRootView() {
-+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-+      }
-+    };
-+  }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
+
+export default createAppContainer(AppNavigator);  
